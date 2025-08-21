b968b64bc16be85689e5a66419e12ee3c759af59
Setepenre <pierre.delaunay.tr@gmail.com>
2025-08-21 08:46:01 -0400

    UV compile python=3.12 (#361)
    * UV compile python=3.12
    
    * Fix rlhf and geo_gnn prepare steps
    
    * Call the right parser
    
    * UV compile python=3.12
    
    * Fix rlhf and geo_gnn prepare steps
    
    * Call the right parser
    
    ---------
    
    Co-authored-by: pierre.delaunay <delaunap@rtx5.server.mila.quebec>
    Co-authored-by: Pierre Delaunay <pierre@delaunay.io>

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.1.1-101-g3eae526"
-__commit__ = "3eae526e37cdc54980866d4516664b195938b9dc"
-__date__ = "2025-08-11 14:09:41 -0400"
+__tag__ = "v1.1.1-113-gc37bfe5"
+__commit__ = "c37bfe570e84378f9598f57692ca32e26c100a07"
+__date__ = "2025-08-18 12:18:52 -0400"
